bd13b7fdf3fd53be8df882a6a7ef5514b4d5a00a
Gabriel Sanches <gabriel@gsr.dev>
2022-06-29 14:10:04 -0300

    Add a more thorough description about the tool

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,13 @@ use clap::Parser;
 
 #[derive(Default, Parser)]
 #[clap(name = "kana")]
-#[clap(about = "A romaji to hiragana/katakana literal converter")]
+#[clap(about = r#"
+Kana is a CLI program for transliterating romaji text to either hiragana or katakana.
+
+Under the hood, it uses a finite-state machine in order to parse the text correctly. It tries to
+implement all standard syllables for both kanas. Most extended syllables for katakana are also
+implemented, being left out only ones that are either obsolete or redundant and older.
+"#)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
 	#[clap(long, short, help = "Transliterate to Katakana")]
